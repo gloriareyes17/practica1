59f0ca3cfb3f921bcce21e8b534d71fa1c7f3b35
gloriareyes17 <82624896+gloriareyes17@users.noreply.github.com>
2021-04-16 00:55:57 -0500

    Primer código subido

---
+++ Proyecto1/Proyecto1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabla = 0;
+            int resultado = 0;
+            do
+            {
+                Console.WriteLine("ingresa la tabla de multiplicar que desea o 0 para salir: ");
+                tabla = int.Parse(Console.ReadLine());
+                for (int j = 0; j <= 10; j++)
+                {
+                    resultado = j * tabla;
+                    Console.WriteLine("La multiplicacion de { 0} por { 1} es { 2}", tabla, j, resultado);
+            }
+            } while (tabla != 0);
+            Console.ReadKey();
+        }
+}
+}
